005bba722d1f5cc7d7837d55250601127c0bb6fe
Younus Ali Akash <younusaliakash8@gmail.com>
2021-03-13 21:09:28 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+// if(firebase.app.length === 0){
+//   firebase.initializeApp(firebaseConfig)
+// }
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  }
 
 function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const [user,setUser] = useState({
+    isSignIn : false,
+    name : '',
+    email : '',
+    photo : ''
+  })
+
+  const handleSignIn = () =>{
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then( res => {
+      const {displayName, email, photoURL} = res.user;
+      const userSignIn ={
+        isSignIn : true,
+        name : displayName,
+        email : email,
+        photo : photoURL
+      }
+      setUser(userSignIn);
+    })
+  }
+
+  const handleSignOut = () =>{
+      const userSignOut ={
+        isSignIn : false,
+        name : '',
+        email : '',
+        photo : ''
+      }
+      setUser(userSignOut);
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignIn ? <button onClick={handleSignOut} >Sign Out</button> : <button onClick={handleSignIn} >Sign In</button>
+      }
+      {
+        user.isSignIn && <div>
+          <p>Dev Akash Authentication System Test</p>
+         <p> Welcome , {user.name}</p>
+         <img src={user.photo} alt=""/>
+        </div>
+      }
+      
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjdI6L-8PlyR7jVlIEDBvqZiKvsy6c7wE",
+    authDomain: "basic-auth-practice.firebaseapp.com",
+    projectId: "basic-auth-practice",
+    storageBucket: "basic-auth-practice.appspot.com",
+    messagingSenderId: "816306463563",
+    appId: "1:816306463563:web:e2cc2729a996e9d4d8fece"
+  };
+
+  export default firebaseConfig;
